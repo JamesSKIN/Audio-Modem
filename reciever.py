76a69144d3ffa9ff8fd70fb44514b939a4496abe
jks70 <jks70@cam.ac.uk>
2024-10-04 18:53:58 +0100

    ALL_files

---
+++ reciever.py
@@ -0,0 +1,386 @@
+# First iteration of a reciever to be used as a library
+
+import ldpc_jossy.py.ldpc as ldpc
+import numpy as np
+from PIL import Image
+from scipy.fft import fft, ifft
+import simpleaudio as sa
+import sounddevice as sd
+from scipy.io.wavfile import write, read
+import scipy
+from transmitter import LDPC
+import matplotlib.pyplot as plt
+from scipy.signal import chirp
+
+QFSK_dictionary = {
+    (1,1) : -1-1j,
+    (1,0) :  1-1j,
+    (0,0) :  1+1j,
+    (0,1) : -1+1j}
+
+
+def pilot_decoded_mag_phase(tds, ofdm):
+    fds = fft(tds, int(ofdm.N))
+    channel_mags = []
+    channel_phases = []
+    
+    for i in range(len(fds)):
+        if i in ofdm.pilot_locs:
+            channel_mags.append(np.abs(fds[i]))
+            channel_phases.append(np.angle(fds[i]))
+    
+    return np.array(channel_mags), np.array(channel_phases)
+
+
+def pilot_channel_estimator(tds, ofdm, interval = np.linspace(0, 1023, 1024)):
+    dcds, angles = pilot_decoded_mag_phase(tds, ofdm)
+    angles = angles % np.pi
+    adcd = np.abs(ofdm.pilot_vals)
+    aangle = np.angle(ofdm.pilot_vals) % np.pi
+
+    mag_fit = scipy.interpolate.CubicSpline(ofdm.pilot_locs, dcds / adcd)
+    ang_fit = scipy.interpolate.CubicSpline(ofdm.pilot_locs, angles - aangle)
+
+    mgf = mag_fit(interval)
+    agf = ang_fit(interval)
+
+    H_est = mgf * np.exp(1j*agf)
+
+    return H_est
+
+
+def pilot_equaliser(tds, ofdm, interval = np.linspace(0, 1023, 1024)):
+    H_est = pilot_channel_estimator(tds, ofdm, interval)
+
+    fds = fft(tds, int(ofdm.N))[:int(ofdm.N / 2)]
+
+    return fds / H_est
+
+
+
+def deconstruct(aud, ofdm, numpy_func = True, channel_H = None, retSymbs = False):
+    N = ofdm.N
+    L = ofdm.CP
+    QFSK_dict  = ofdm.QFSK_dict
+    backwards_dict = {v : k for k, v in QFSK_dict.items()}
+
+    bits_organised = aud.reshape((-1, L+N))
+
+    cut_bits = bits_organised[:,L:]
+
+    if numpy_func == True:
+        freq_data = np.fft.fft(cut_bits)
+    else:
+        freq_data = fft(cut_bits)
+
+
+    symbols = freq_data[:,1:int(N/2)]
+
+    if channel_H is None:
+        pass
+    else:
+        symbols = symbols / channel_H
+
+    soliddata=[]
+    for i in symbols:
+        for j in i:
+            if j.real > 0 and j.imag > 0:
+                soliddata.extend([backwards_dict[1+1j][0],backwards_dict[1+1j][1]])
+            elif j.real < 0 and j.imag > 0:
+                soliddata.extend([backwards_dict[-1+1j][0],backwards_dict[-1+1j][1]])
+            elif j.real > 0 and j.imag < 0:
+                soliddata.extend([backwards_dict[1-1j][0],backwards_dict[1-1j][1]])
+            else:
+                soliddata.extend([backwards_dict[-1-1j][0],backwards_dict[-1-1j][1]])
+
+    # ldpc = LDPC(rate = ofdm.rate, z = ofdm.z)
+    # decoded = ldpc.encode(soliddata)
+    
+    if retSymbs == True:
+        return np.array(soliddata), symbols
+    else:
+        return np.array(soliddata)
+    
+
+def bitsToSaveImage(bit_array, image_name, x, y):
+    # turn back into a byte array
+    abc = [int((''.join(str(k) for k in j)),base=2) for j in bit_array.reshape(-1,8)]
+    # Turn array into form PIL likes
+    abc = np.array(abc).reshape((x, y, -1 ))  
+    abc = Image.fromarray(abc.astype(np.uint8))
+
+    img = Image.fromarray(np.array(abc))
+
+    img.save('{}.tiff'.format(image_name))
+
+
+def unrep(datastream):
+    array_out = []
+    for i in range(0,len(datastream),3):
+        if datastream[i]+datastream[i+1]+datastream[i+2] >= 2:
+            array_out.append(1)
+        else:
+            array_out.append(0)
+    return np.array(array_out)
+
+
+def bitArrayToText(bytes_as_bits):
+    abc = bytes_as_bits.reshape(-1,8).astype(str)
+    abc = [("".join(x)) for x in abc]
+    abc = [int(x,base=2) for x in abc]
+    abc_bytea = bytearray(abc)
+    print(abc_bytea.decode("latin-1"))
+
+
+def record(timeofrec,fs,filename):
+
+    sd.default.samplerate = fs
+    sd.default.channels = 1
+
+    myrecording = sd.rec(int(timeofrec * fs))
+    sd.wait()  # Wait until recording is finished
+
+    write('{}.wav'.format(filename), fs, myrecording)  # Save as WAV file
+
+
+def extractor(symbols, ofdm):
+    indices = [i for i in range(ofdm.pilot_locs[0], ofdm.pilot_locs[-1]) if i not in ofdm.pilot_locs]
+    return symbols[:,indices]
+
+def snc_extractor(symbols):
+    l = symbols.shape[0]
+    a1 = np.arange(60, l, 62)
+    a2 = np.arange(61, l, 62)
+    at = np.concatenate((a1,a2))
+    sc_indices = [i for i in range(l) if i not in at] 
+
+    return symbols[sc_indices]
+
+
+def standard_deconstructor(aud, ofdm, channel_H = None, multi_est=False, retSymbs = False, ldpc_encoded = True, add_rotate=None):
+    N = ofdm.N
+    L = ofdm.CP
+    QFSK_dict  = ofdm.QFSK_dict
+    backwards_dict = {v : k for k, v in QFSK_dict.items()}
+
+    bits_organised = aud.reshape((-1, L+N))
+
+    snc_removed = snc_extractor(bits_organised)
+
+    cut_bits = snc_removed[:,L:]
+
+    freq_data = fft(cut_bits)
+
+    # symbols = freq_data[:,1:int(N/2)]
+
+    # equalisation
+
+    if channel_H is None:
+        pass
+    elif multi_est == False:
+        freq_data = freq_data / channel_H
+    else:
+        item=0
+        for i in range(0,len(freq_data),60):
+            try:
+                print(i,item)
+                freq_data[i:i+60] = freq_data[i:i+60] / channel_H[item]
+                item+=1
+            except:
+                print(i,item,np.shape(freq_data))
+                freq_data[i:] = freq_data[i:] / channel_H[item]
+                item+=1
+
+
+    symbols = extractor(freq_data, ofdm)
+
+    if add_rotate is not None:
+        symbols = symbols * np.exp(add_rotate)
+
+    soliddata=[]
+    for i in symbols:
+        for j in i:
+            if j.real > 0 and j.imag > 0:
+                soliddata.extend([backwards_dict[1+1j][0],backwards_dict[1+1j][1]])
+            elif j.real < 0 and j.imag > 0:
+                soliddata.extend([backwards_dict[-1+1j][0],backwards_dict[-1+1j][1]])
+            elif j.real > 0 and j.imag < 0:
+                soliddata.extend([backwards_dict[1-1j][0],backwards_dict[1-1j][1]])
+            else:
+                soliddata.extend([backwards_dict[-1-1j][0],backwards_dict[-1-1j][1]])
+
+    if ldpc_encoded ==True:
+        coder = LDPC(rate = ofdm.rate, z = ofdm.z)
+        decoded = coder.decode(np.array(soliddata))
+
+        decoded = np.int64(decoded).flatten()
+    else:
+        decoded = np.array(soliddata)
+    
+    if retSymbs == True:
+        return decoded, symbols
+    else:
+        return decoded
+    
+def SchmidlCoxDecoder(audio,ofdm):
+    dest = np.arange(0, len(audio))
+    P1 = np.zeros(len(dest), dtype=complex)
+    for i, d in enumerate(dest):
+        P1[i] = sum(audio[d+m].conj()*audio[d+m+int(ofdm.N//2)] for m in range(int(ofdm.N//2))) 
+    
+    R = np.zeros(len(dest))
+    for i, d in enumerate(dest):
+        R[i] = sum(abs(audio[d+m+int(ofdm.N//2)])**2 for m in range(int(ofdm.N//2)))
+
+    M = abs(P1)**2/R**2
+
+    b_toPeak = np.ones(ofdm.CP) / ofdm.CP
+    a = (1,)
+    M_filt = scipy.signal.lfilter(b_toPeak, a, M)
+    return M_filt
+
+
+def channelEstimate(four_known_symbols, ofdm):
+
+    even_one = four_known_symbols[:ofdm.N+ofdm.CP]
+    odd_one = four_known_symbols[ofdm.N+ofdm.CP:2*(ofdm.N+ofdm.CP)]
+    even_two = four_known_symbols[2*(ofdm.N+ofdm.CP):3*(ofdm.N+ofdm.CP)]
+    odd_two = four_known_symbols[3*(ofdm.N+ofdm.CP):4*(ofdm.N+ofdm.CP)]
+    odd_index = np.arange(0,741,2)
+    even_index=np.arange(1,741,2)
+
+    exported_coding = np.loadtxt('preamble_qpsk_symbols.csv', delimiter=',',dtype=complex)
+    exported_coding *= 1.41421356474619
+
+    hes_e1 = (fft(even_one[ofdm.CP:], 2048)).flatten()[49:790] / exported_coding.flatten()
+    hes_o1 = (fft(odd_one[ofdm.CP:], 2048)).flatten()[49:790] / exported_coding.flatten()
+    hes_e2 = (fft(even_two[ofdm.CP:], 2048)).flatten()[49:790] / exported_coding.flatten()
+    hes_o2 = (fft(odd_two[ofdm.CP:], 2048)).flatten()[49:790] / exported_coding.flatten()
+    
+    hest_one = np.zeros(741, dtype='complex')
+    hest_one[even_index] = hes_e1[even_index]
+    hest_one[odd_index] = hes_o1[odd_index]
+
+    hest_two = np.zeros(741, dtype='complex')
+    hest_two[even_index] = hes_e2[even_index]
+    hest_two[odd_index] = hes_o2[odd_index]
+
+
+    return hest_one, hest_two
+
+
+def twoSymbCE(two_known_symbols, ofdm):
+    even_one = two_known_symbols[:ofdm.N+ofdm.CP]
+    odd_one = two_known_symbols[ofdm.N+ofdm.CP:2*(ofdm.N+ofdm.CP)]
+    odd_index = np.arange(0,741,2)
+    even_index=np.arange(1,741,2)
+
+    exported_coding = np.loadtxt('preamble_qpsk_symbols.csv', delimiter=',',dtype=complex)
+    exported_coding *= 1.41421356474619
+    
+    hes_e1 = (fft(even_one[ofdm.CP:], 2048)).flatten()[49:790] / exported_coding.flatten()
+    hes_o1 = (fft(odd_one[ofdm.CP:], 2048)).flatten()[49:790] / exported_coding.flatten()
+
+    hest = np.zeros(741, dtype='complex')
+    hest[even_index] = hes_e1[even_index]
+    hest[odd_index] = hes_o1[odd_index]
+
+    return hest
+
+
+
+def chanest_padd(hest,ofdm):
+    full_size = np.ones(ofdm.N, dtype='complex')
+    full_size[49:790] = hest
+    return full_size
+
+def error_(data1,data_correct):
+    errs = 0
+    for i in range(len(data_correct)):
+        if data_correct[i] != data1[i]:
+            errs += 1
+    return 100*errs/len(data_correct)
+
+def errorss(decod, bit_array):
+    code_length = len(bit_array)
+    error_by_block = []
+    n=720*2
+    for i in range(0,code_length,n):
+        error_by_block.append(error_(decod[i:i+n],bit_array[i:i+n]))
+    return error_by_block
+
+
+def chirpEnds(signal, note=None, graph_display=False):
+
+    if note is None:
+        chirp_time = 1
+        fs =44100
+        t = np.linspace(0, chirp_time, int(chirp_time * fs), False)
+        note = chirp(t, f0=250, f1=20000, t1=chirp_time, method='linear')
+        note = note*np.hamming(len(note))
+
+    norm_signal =  signal/np.max(signal)
+    peka = scipy.signal.find_peaks(norm_signal, 0.4)[0]
+    if peka[0] <= 10000:
+        start_search1 = 0
+    else:
+        start_search1 = peka[0]-10000
+    end_search1 = start_search1 + 100000
+
+    if len(norm_signal) - peka[-1] <= 25000:
+        end_search2 = len(norm_signal)-1
+    else:
+        end_search2 = peka[-1]+25000
+    start_search2 = end_search2 - 100000
+
+    print(peka[-1],start_search2,end_search2,len(signal),start_search1,end_search1)
+
+    delay_guess1 = np.abs(np.correlate(signal[start_search1:end_search1], note, mode='full'))
+    delay_guess2 = np.abs(np.correlate(signal[start_search2:end_search2], note, mode='full'))
+
+    delay_guess_norm1 = delay_guess1/np.max(delay_guess1)
+    delay_guess_norm2 = delay_guess2/np.max(delay_guess2)
+
+    peaks_1 = scipy.signal.find_peaks(delay_guess_norm1, 0.5, distance = 3000)
+    peaks_2 = scipy.signal.find_peaks(delay_guess_norm2, 0.5, distance = 3000)
+
+    end_first_chirp = peaks_1[0][0]+start_search1
+    end_second_chirp = peaks_1[0][1]+start_search1
+    end_third_chirp = peaks_2[0][0]+start_search2
+    end_fourth_chirp = peaks_2[0][1]+start_search2
+    
+    if graph_display == True:
+        plt.figure(figsize = (30, 10))
+        plt.plot(signal)
+        plt.axvline(x = start_search1 , color = 'grey',linestyle = '-', label='start_search')
+        plt.axvline(x = end_search1 , color = 'grey',linestyle = '--', label='end_search')
+        plt.axvline(x = start_search2 , color = 'grey',linestyle = '-', label='start_search')
+        plt.axvline(x = end_search2 , color = 'grey',linestyle = '--', label='end_search')
+
+        plt.axvline(x = end_first_chirp , color = 'r', label='end_first_chirp')
+        plt.axvline(x = end_second_chirp , color = 'b', label='end_second_chirp')
+        plt.axvline(x = end_third_chirp , color = 'r', label='end_third_chirp')
+        plt.axvline(x = end_fourth_chirp , color = 'b', label='end_fourth_chirp')
+
+        plt.legend()
+        plt.show()
+
+
+    return [end_first_chirp,end_second_chirp, end_third_chirp, end_fourth_chirp]
+
+def decode_header(bits_in):
+    lst = [str(i) for i in bits_in[:7*8*3]]
+    out = [int(''.join(map(str, lst[i:i+8])),2) for i in range(0, len(lst), 8)]
+    bytes_in = bytearray(out)
+
+    verified_header=b''
+    for i in range(7):
+        if bytes_in[i] == bytes_in[i+7] or bytes_in[i] == bytes_in[i+14]:
+            verified_header += bytes_in[i].to_bytes(1, byteorder='big')
+        elif bytes_in[i+7] == bytes_in[i+14]:
+            verified_header += bytes_in[i+7].to_bytes(1, byteorder='big')
+        else:
+            raise ValueError("There are too many errors in the input header")
+    filetype = '.' + verified_header[0:4].strip(b'\x00').decode('utf-8')
+    filesize = int.from_bytes(verified_header[4:7], 'big')
+    return filetype, filesize
---
+++ transmitter.py
@@ -0,0 +1,221 @@
+# First iteration of functional transmitter to be used as a library
+
+
+import ldpc_jossy.py.ldpc as ldpc
+import numpy as np
+import simpleaudio as sa
+from scipy.signal import chirp
+import pyaudio
+import sounddevice as sd
+from scipy.io.wavfile import write, read
+from PIL import Image
+from scipy.fft import fft, ifft
+
+# Default QFSK constellation
+QFSK_dictionary = {
+    (1,1) : -1-1j,
+    (1,0) :  1-1j,
+    (0,0) :  1+1j,
+    (0,1) : -1+1j}
+
+class LDPC:
+    def __init__(self, rate, z) -> None:
+        self.rate = rate
+        self.z = z
+        self.coder = ldpc.code(rate = rate, z = z)
+
+    def encode(self, bin_seq):
+        Y = self.coder.pcmat().shape[0]
+        remainder = Y - (len(bin_seq) % Y)
+        coerced = np.concatenate((bin_seq, np.zeros(remainder)))
+        coerced = coerced.reshape(-1, self.coder.pcmat().shape[0])
+        return np.array([self.coder.encode(c) for c in coerced])
+
+    def decode(self, bin_seq):
+        bin_seq = bin_seq.flatten()
+        print(bin_seq.shape)
+        print(len(bin_seq)-(len(bin_seq) % self.coder.N))
+        bin_seq = bin_seq[:len(bin_seq)-(len(bin_seq) % self.coder.N)]
+        print(bin_seq.shape)
+        recoerced = bin_seq.reshape(-1, self.coder.pcmat().shape[1])
+        recoerced = 10 * (0.5 - recoerced)
+        stepper = np.array([self.coder.decode(rc)[0] for rc in recoerced])[:,:self.coder.K]
+        for i in range(len(stepper)):
+            for j in range(len(stepper[0])):
+                if stepper[i][j] < 0:
+                    stepper[i][j] = 1
+                else:
+                    stepper[i][j] = 0
+        return stepper
+
+
+# Takes an array of bits and converts it to a 1d array of symbols
+def bit2symbol(x, ofdm):
+    bits_organised = x.reshape((-1, ofdm.bps)) # organise the bits into lengths of bps
+    return np.array([ofdm.QFSK_dict[tuple(b)] for b in bits_organised]) 
+
+# Takes array of symbols and turns it into blocks of size N.
+# Will add zeros on the end of the last block if it does not divide nicly. --- need to work on what to add on the end ---
+def cut2Blocks(data, ofdm):
+    if np.shape(data)[0]%int(ofdm.spb) == 0:
+        return data.reshape(-1,int(ofdm.spb))
+    else:
+        new_data = np.concatenate((data, np.zeros(ofdm.spb - np.shape(data)[0]%int(ofdm.spb))))
+        return new_data.reshape(-1,int(ofdm.spb))
+
+
+def addpilots(data, ofdm):
+    di = 0
+    pi = 0
+    plted = np.zeros(ofdm.N // 2 - 1, dtype=complex)
+    for i in range(ofdm.pilot_locs[0], ofdm.pilot_locs[-1] + 1):
+        # offset of 1 needed for original functioning of goodSymbols to be retained.
+        if i in ofdm.pilot_locs:
+            plted[i-1] = ofdm.pilot_vals[pi]
+            pi += 1
+        else:
+            plted[i-1] = data[di]
+            di += 1
+
+    return plted
+
+
+
+
+def addpadding(data, ofdm):
+    qfsk = np.array(list(ofdm.QFSK_dict.values()))
+    padding_low = np.random.choice(qfsk, size=(int(ofdm.start_bin-1)), replace=True)
+    padding_high = np.random.choice(qfsk, size=(int(ofdm.N/2-ofdm.end_bin)), replace=True)
+
+    padding_low = np.vstack(np.array([padding_low for i in range(data.shape[0])]))
+    padding_high = np.vstack(np.array([padding_high for i in range(data.shape[0])]))
+    
+    return np.hstack((padding_low, data, padding_high))
+
+
+# Takes the blocks of symbols and adds the mirrored conjugate onto the end of the blocks
+def goodSymbols(data_symbols, ofdm):
+    symbols = np.zeros((np.shape(data_symbols)[0],ofdm.N), dtype=complex)
+    for i in range(symbols.shape[0]):
+        piloted = addpilots(data_symbols[i],ofdm)
+
+        for j in range(1, int(ofdm.N/2)):
+            symbols[i][j] = piloted[j-1]
+            symbols[i][int(ofdm.N)-j] = np.conjugate(piloted[j-1])
+    return symbols
+
+
+# inverse dft, default numpy, can be chanegd to scipy
+# For some reason the scipy was leaving tiny(e-18) imaginary parts
+def inversedft(freq, numpy_func = True, N = 2048):
+    if numpy_func == True:
+        return np.real(np.fft.ifft(freq, N))
+    else:
+        return np.real(ifft(freq, N))
+
+# Adds the cyclic prefix
+def addGuard(timeDomain, ofdm):
+    print(ofdm.CP)
+    return np.hstack((timeDomain[:,-ofdm.CP:], timeDomain))
+
+def bitsFromTiff(image_name):
+    # Open image
+    image_file = Image.open(image_name)
+    image_file_array = np.array(image_file)
+    # make into a 1d binary array
+    long_array = image_file_array.reshape(1,-1) # Decimal bytes
+    bin_byte_array = ["{0:08b}".format(i) for i in long_array[0]] # Binary bytes
+    
+    return np.array([bity for bytey in bin_byte_array for bity in bytey]).astype(int)
+
+def bitsFromTxt(file):
+    f = open(file, 'rb')
+    content = f.read()
+    return np.array([int(i) for i in ''.join([(format(byte, '08b')) for byte in content])])
+
+
+def repetionCode(binary, k=3):
+    return np.array([element for element in binary for i in range(k)])
+
+
+
+def frameMaker(sync, chan_est, data, data_symb_per_frame, zeros_post_sync=np.empty((0,), dtype=int), zeros_post_sybm=np.empty((0,), dtype=int)):
+    initial_data_length = len(data)
+    message = []
+    for i in range(0, initial_data_length, data_symb_per_frame):
+        indiv_frame = np.concatenate((sync.flatten(),zeros_post_sync,chan_est,data[i:i+data_symb_per_frame].flatten(),zeros_post_sybm))
+        message.append(indiv_frame)
+        #data = np.insert(data,i,np.concatenate((sync.flatten(),zeros_post_sync,chan_est,old_data[i:i+data_symb_per_frame].flatten(),zeros_post_sybm)))
+    #data = np.insert(data, range(0, len(data), data_symb_per_frame), np.concatenate((sync,zeros_post_sync,chan_est,data[i:i+data_symb_per_frame].flatten(),zeros_post_sybm)))
+    return np.array(message)
+
+
+
+def fullTrans(data, ofdm, ldpc_encode = True):
+    if ldpc_encode == True:
+        # ldpc implementation
+        ldpc = LDPC(rate = ofdm.rate, z = ofdm.z)
+        ldpc_ified = ldpc.encode(data)
+    else:
+        ldpc_ified = data
+    
+    symb = bit2symbol(ldpc_ified, ofdm)
+
+    cut_symb = cut2Blocks(symb, ofdm)
+    # syb_padded = addpadding(cut_symb, ofdm)
+    all_symbs = goodSymbols(cut_symb,ofdm)
+    tds = np.real(ifft(all_symbs, 2048))
+    return addGuard(tds, ofdm)
+
+def audioMaker(frame, name, fs):
+    audio = frame.flatten() / np.max(np.abs(frame.flatten()))
+    audio_for_file = audio * (2**15 - 1) / np.max(np.abs(audio))
+    audio_for_file = audio_for_file.astype(np.int16)
+    write('{}.wav'.format(name), fs, audio_for_file)
+    return audio_for_file
+
+def gen_header(filename, filesize):
+    filetype = filename.split('.')[-1]
+    if len(filetype) > 4:
+        raise ValueError(f"File extension .{filetype} is greater than 4 characters long")
+    if filesize > 2**24-1:
+        raise ValueError("Filesize too large")
+    return 3 * bytearray(bytearray(filetype,'utf-8').ljust(4, b'\x00') + int(filesize).to_bytes(length=3,byteorder='big'))
+
+def double_chirp(fs=44100):
+    chirp_time = 1
+    t = np.linspace(0, chirp_time, int(chirp_time * fs), False)
+    note = chirp(t, f0=250, f1=20000, t1=chirp_time, method='linear')
+    note = note*np.hamming(len(note))
+    note = np.concatenate((np.zeros(1500),note,note,np.zeros(1500)))
+    return note / np.max(np.abs(note))
+    
+
+def rand_snc(ofdm):
+    qfsk = np.array(list(ofdm.QFSK_dict.values()))
+    return np.random.choice(qfsk, size=(int(ofdm.N/2-1)), replace=True)
+    ####### to be continueeed
+
+
+def s_n_c(rands, ofdm):
+    cut_symb = cut2Blocks(symb, ofdm)
+    syb_padded = addpadding(cut_symb, ofdm)
+    all_symbs = goodSymbols(syb_padded,ofdm)
+    preamble_array[::2] = 0
+    ####### to be continueeed
+
+def CUSTARD(payload,ofdm):
+    chirp = double_chirp().flatten()
+    exported_even, exported_odd = np.loadtxt('preamble_ofdm_symbols.csv',delimiter=',')
+    two_snc = np.concatenate((exported_even, exported_odd)).flatten()
+    four_snc = np.concatenate((two_snc, two_snc)).flatten()
+
+    transmission = np.concatenate((chirp,four_snc))
+    for i in range(len(payload)//ofdm.sfp):
+        transmission = np.concatenate((transmission, payload[i*60:60*i+60].flatten(), two_snc))
+    if len(payload)%ofdm.sfp != 0:
+        transmission = np.concatenate((transmission, payload[-(len(payload)%ofdm.sfp):].flatten(), four_snc, chirp))
+    else:
+        transmission = np.concatenate((transmission, four_snc, chirp))
+    
+    return transmission
